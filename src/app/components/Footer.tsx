5087f05d37ef1320e95755223d37136ae008b9ca
tyzrex <sulavbaral58@gmail.com>
2023-12-02 22:39:24 +0545

    Add Footer and Navbar components

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,50 @@
+import { Facebook, Instagram, Linkedin, Mail, Twitter } from "lucide-react";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <>
+      <footer className="text-orange-100">
+        <div className="mx-auto w-full max-w-screen-2xl  px-10 sm:px-6 lg:px-8 py-16 md:py-24 lg:py-32">
+          <div className="flex justify-between items-end">
+            <div className="flex flex-col md:flex-row md:justify-between md:items-center">
+              <div className="flex flex-col gap-5 mb-10 md:mb-0">
+                <Link href="/">
+                  <h1 className="text-3xl font-bold">{"< SB />"}</h1>
+                </Link>
+                <p className="text-3xl">Sulav Baral</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-end">
+              <Link href="#" className="mb-8 inline-block max-w-full"></Link>
+
+              <div>
+                <div className="mb-12 grid w-full grid-flow-col grid-cols-4 gap-8">
+                  <Link href="#">
+                    <Facebook size={32} />
+                  </Link>
+                  <Link href="#">
+                    <Instagram size={32} />
+                  </Link>
+                  <Link href="#">
+                    <Linkedin size={32} />
+                  </Link>
+                  <Link href="#">
+                    <Twitter size={32} />
+                  </Link>
+                  <Link href="#">
+                    <Mail size={32} />
+                  </Link>
+                </div>
+              </div>
+              <p className="max-[479px]:text-sm text-3xl">
+                Sulav Baral © Copyright 2023. All rights reserved.
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,61 @@
+import Link from "next/link";
+
+import MobileMenu from "./Mobile-Menu";
+
+const links = [
+  { href: "/", label: "Home" },
+  { href: "/about", label: "About" },
+  { href: "/experience", label: "Experience" },
+  { href: "/projects", label: "Projects" },
+  { href: "/contacts", label: "Contacts" },
+];
+
+export default function Navbar() {
+  return (
+    <>
+      <header>
+        <div className="mx-auto max-w-screen-2xl px-10 sm:px-6 lg:px-8">
+          <div className="flex h-24 items-center justify-between">
+            <div className="md:flex md:items-center md:gap-12">
+              <Link className="block text-orange-100" href="/">
+                <h1 className="text-3xl font-bold">{"< SB />"}</h1>
+              </Link>
+            </div>
+
+            <div className="hidden md:block">
+              <nav aria-label="Global">
+                <ul className="flex items-center gap-10 text-md ">
+                  {links.map(({ href, label }) => (
+                    <li key={`${href}${label}`}>
+                      <Link
+                        className="text-orange-100 hover:text-orange-300 color-transition nav-links"
+                        href={href}
+                      >
+                        {label}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            </div>
+
+            <div className="flex items-center gap-4">
+              <div className="sm:flex sm:gap-4">
+                <div className="hidden sm:flex">
+                  <Link
+                    className="rounded-md bg-zinc-800 px-5 py-2.5 text-sm font-medium text-orange-100"
+                    href="/"
+                  >
+                    Let&apos;s talk
+                  </Link>
+                </div>
+              </div>
+
+              <MobileMenu />
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
